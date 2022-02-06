d141b6a62d3e690cdf1ed68a55c8be26ad2567c6
veraueda <veraueda@hotmail.com>
2022-02-06 01:23:55 -0300

    Reusing instantiation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -5,7 +5,7 @@ import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
-	public static SellerDao createSalaryDao() {
+	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
