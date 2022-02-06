648db0c8adbf03b7401ced50052a8f4f5e129228
veraueda <veraueda@hotmail.com>
2022-02-06 00:32:02 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,7 +14,9 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj); 
-				
+		
+		SellerDao sellerDao = DaoFactory.createSalaryDao();
+		
 		System.out.println(seller);
 
 	}
